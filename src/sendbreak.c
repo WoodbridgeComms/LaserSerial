7af9d449025be8f9c8ae6a6d1d75680abe6b6d62
sethferrell <sethferrell@gmail.com>
2022-02-12 15:36:29 -0800

    Reorganize files + add old sendbreak

---
+++ src/sendbreak.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <termios.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  int fd, duration;
+  if (argc != 2)
+  {
+    printf("Argument error: input requires one integer argument");
+    exit(EXIT_FAILURE);
+  }
+  
+  duration = 1000 * atoi(argv[1]);
+
+  if ((fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY)) == -1)
+  {
+    printf("open_port: Unable to open /dev/ttyUSB0");
+    exit(EXIT_FAILURE);
+  }
+  else
+  {
+    tcsendbreak(fd, duration);
+    printf("Successfully sent %d sec BREAK to /dev/ttyUSB0", atoi(argv[1]));
+    close(fd);
+  }
+  
+  return EXIT_SUCCESS;
+}
