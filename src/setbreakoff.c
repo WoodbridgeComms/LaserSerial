fe0030c4fb0f002b0f04344052d86ae4cb4559e7
sethferrell <sethferrell@gmail.com>
2022-02-12 16:12:49 -0800

    Add permanent break conditions

---
+++ src/setbreakoff.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+
+int main()
+{
+  int fd;
+
+  if ((fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY)) == -1)
+  {
+    printf("open_port: Unable to open /dev/ttyUSB0");
+    exit(EXIT_FAILURE);
+  }
+  else
+  {
+    ioctl(fd, TIOCCBRK, NULL);
+    printf("Successfully set BREAK OFF");
+    close(fd);
+  }
+  
+  return EXIT_SUCCESS;
+}
